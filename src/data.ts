3c6c2c13cc15cb25227ef1c9dfb47db45e968104
santiagomac <smazuerac@unac.edu.co>
2024-11-21 11:08:21 -0500

    add portfolio and move the links to an array

---
+++ src/data.ts
@@ -0,0 +1,19 @@
+export const links = [
+  {
+    title: "Portfolio",
+    href: "https://santiagomac.pages.dev",
+  },
+  {
+    title: "Github",
+    href: "https://github.com/santiagomac",
+  },
+
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/santiagomazuera/",
+  },
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/santimazuers/",
+  },
+];
